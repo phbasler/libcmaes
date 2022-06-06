9497ac2ce087606c4d28af4f476fd159ee24a847
Philipp Basler <philipp.basler@kit.edu>
2022-06-06 11:36:31 +0200

    update

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -66,6 +66,12 @@ class libcmaesConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        
         cmake = CMake(self)
         cmake.install()
 
